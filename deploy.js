75403947bb253a28e707b9d0b1d6f3a6a153be90
Christopher Slater <chris@snapper.fish>
2016-12-03 14:48:01 +0000

    Add configuration for travis builds

---
+++ deploy.js
@@ -0,0 +1,14 @@
+var ghpages = require('gh-pages');
+var path = require('path');
+
+// var repo = 'https://github.com/snapperfish-company/snapperfish-company.github.io.git';
+var repo = 'https://d1746a843f19be67690f16b812790c7ddcea2fcd@github.com:snapperfish-company/snapperfish-company.github.io.git';
+
+function callback() {
+  console.log('Deployed!!', arguments);
+}
+
+ghpages.publish(path.join(__dirname, 'public'), {
+  repo: repo,
+  branch: 'master',
+}, callback);
