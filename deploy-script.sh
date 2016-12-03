acac17ed33fe51ecdef9209f268ddedfa6b6801a
Christopher Slater <chris@snapper.fish>
2016-12-03 21:31:27 +0000

    w00t

--- deploy-script.sh
+++ deploy-script.sh
@@ -17,4 +17,4 @@ cd $TRAVIS_BUILD_DIR/public
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://$GH_TOKEN@github.com/snapperfish-company/snapperfish-company.github.io.git" master:master > /dev/null 2>&1
+git push --force "https://$GH_TOKEN@github.com/snapperfish-company/snapperfish-company.github.io.git" master:master > /dev/null 2>&1
